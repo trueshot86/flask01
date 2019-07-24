d108b09c27dae597a22d8025a81822df388eabc7
ezreal <xfarm001@gmail.com>
2019-07-25 04:17:18 +0900

    this is standard

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask,request,redirect,url_for,render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello World"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='192.168.100.45',port=80)
